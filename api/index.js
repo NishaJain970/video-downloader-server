adc0cca4dabe722990d16724cf7187d621f8b281
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-11 10:23:53 +0530

    testing

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/api", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello /api!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello /!");
 });
 
 app.listen(port, () => {
