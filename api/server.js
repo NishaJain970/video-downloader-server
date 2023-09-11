07cf0d96803c5de1336e942e4f7a49735557091e
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-11 10:07:05 +0530

    Configuring fb video/reel downloader

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const getFBInfo = require("@xaviabot/fb-downloader");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Functions
+async function printFBInfo() {
+  try {
+    const result = await getFBInfo("https://fb.watch/m_jEGodCb7/");
+    return result;
+    // console.log("Result:", result);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+app.get("/", async (req, res) => {
+  const resultData = await printFBInfo();
+  res.send(resultData);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
