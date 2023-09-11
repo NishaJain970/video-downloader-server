c8dddcc3de279f80cba89d6579b426f1950d9777
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-11 10:27:38 +0530

    Added /api/fb to the api list

--- api/index.js
+++ api/index.js
@@ -2,12 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const getFBInfo = require("@xaviabot/fb-downloader");
+
+// functions
+
+async function printFBInfo() {
+  try {
+    const result = await getFBInfo(
+      "https://www.facebook.com/watch?v=272591278381388"
+    );
+    return result;
+    // console.log("Result:", result);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
 app.get("/api", (req, res) => {
-  res.send("Hello /api!");
+  res.send("Hello Video Downloader API !");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello /!");
+app.get("/api/fb", async (req, res) => {
+  const resData = await printFBInfo();
+  res.send(resData);
 });
 
 app.listen(port, () => {
