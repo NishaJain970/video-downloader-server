994fdaaae474cecdd3f77a4e5bcd8966ff595563
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-11 10:15:41 +0530

    Moved back to full EXPRESS

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
 const express = require("express");
-// const cors = require("cors");
-
-// const getFBInfo = require("@xaviabot/fb-downloader");
-
 const app = express();
 const port = 3000;
 
-// app.use(cors());
-
-// Functions
-// async function printFBInfo() {
-//   try {
-//     const result = await getFBInfo("https://fb.watch/m_jEGodCb7/");
-//     return result;
-//     // console.log("Result:", result);
-//   } catch (error) {
-//     console.log("Error:", error);
-//   }
-// }
-
 app.get("/", (req, res) => {
-  // const resultData = await printFBInfo();
-  res.send("Hello");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
