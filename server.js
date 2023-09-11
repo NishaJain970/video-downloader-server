31b6155b33433d605027fcba26250c0047720152
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-11 10:14:08 +0530

    Only express

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
-const getFBInfo = require("@xaviabot/fb-downloader");
+// const getFBInfo = require("@xaviabot/fb-downloader");
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// app.use(cors());
 
 // Functions
-async function printFBInfo() {
-  try {
-    const result = await getFBInfo("https://fb.watch/m_jEGodCb7/");
-    return result;
-    // console.log("Result:", result);
-  } catch (error) {
-    console.log("Error:", error);
-  }
-}
+// async function printFBInfo() {
+//   try {
+//     const result = await getFBInfo("https://fb.watch/m_jEGodCb7/");
+//     return result;
+//     // console.log("Result:", result);
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
+// }
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   // const resultData = await printFBInfo();
   res.send("Hello");
 });
