e06186e8456835a709e811104e8a7ee79945015c
Samyak Jain <70690987+Sammy970@users.noreply.github.com>
2024-10-06 12:19:58 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,71 +1,60 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import getFBInfo from "@xaviabot/fb-downloader";
+import { snapsave } from "snapsave-media-downloader";
+
 const app = express();
 const port = 3001;
 
-// Facebook
-const getFBInfo = require("@xaviabot/fb-downloader");
-
-// const { igdl } = require("ruhend-scraper");
-
-// CORS Policy
-const cors = require("cors");
 app.use(cors());
 
-// functions
 async function printFBInfo(video) {
   try {
     const result = await getFBInfo(video);
     return result;
-    // console.log("Result:", result);
   } catch (error) {
     console.log("Error:", error);
   }
 }
 
-// async function fetchInstagramMedia(url) {
-//   try {
-//     let res = await igdl(url);
-//     let data = await res.data;
+async function fetchInstagramMedia(url) {
+  try {
+    const download = await snapsave(url);
 
-//     if (data.length > 0) {
-//       const res = {
-//         data: [
-//           {
-//             url: data[0].url,
-//           },
-//         ],
-//       };
+    if (download.status === true) {
+      const res = {
+        data: [
+          {
+            url: download?.data?.length > 0 ? download.data[0].url : "",
+          },
+        ],
+      };
 
-//       return res;
-//     }
-//   } catch (error) {
-//     console.error("Bir hata oluştu:", error);
-//   }
-// }
+      return res;
+    }
+  } catch (error) {
+    console.error("Bir hata oluştu:", error);
+  }
+}
 
-// Test API
 app.get("/api", (req, res) => {
   res.send("Hello Video Downloader API !");
 });
 
-// Facebook API
 app.get("/api/fb", async (req, res) => {
   const video = req.query.video;
-  // console.log(video);
   const resData = await printFBInfo(video);
   res.json(resData);
 });
 
-// Instagram API
-// app.get("/api/insta", async (req, res) => {
-//   const link = req.query.link;
-//   // let resData = await idl(link);
-//   let resData = await fetchInstagramMedia(link);
-//   res.json(resData);
-// });
+app.get("/api/insta", async (req, res) => {
+  const link = req.query.link;
+  let resData = await fetchInstagramMedia(link);
+  res.json(resData);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-module.exports = app;
+export default app;
